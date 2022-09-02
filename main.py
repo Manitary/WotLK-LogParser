8eb9ee735f496b528bd75af9e011deeceaf8c659
Manitary <pak.ska@gmail.com>
2022-09-02 21:24:27 +0100

    Add global buff uptime

--- main.py
+++ main.py
@@ -322,7 +322,7 @@ class MainWindow(QMainWindow):
             display_query.bindValue(":startTime", self.encounter_select.currentData()[0])
             display_query.exec()
             self.table.setModel(deathSqlTableModel(display_query))
-            for i in range(5, 9):
+            for i in range(5, 10):
                 self.table.hideColumn(i)
         else:
             with open('queries/deaths_1.sql', 'r') as f:
@@ -338,52 +338,57 @@ class MainWindow(QMainWindow):
 
     def queryBuffs(self):
         everyone = self.source_select.currentData() == AFFILIATION[self.source_affiliation]
-        if not everyone:
-            self.table.setItemDelegateForColumn(2, auraDelegate())
-            startTime = self.encounter_select.currentData()[0]
-            endTime = self.encounter_select.currentData()[1]
-            q = QSqlQuery()
+        #if not everyone:
+        self.table.setItemDelegateForColumn(2, auraDelegate())
+        startTime = self.encounter_select.currentData()[0]
+        endTime = self.encounter_select.currentData()[1]
+        q = QSqlQuery()
+        if everyone:
+            with open('queries/buffs_taken_all-all.sql', 'r') as f:
+                q.prepare(f.read())
+                q.bindValue(":affiliation", self.source_affiliation)
+        else:
             with open('queries/buffs_taken_1-all.sql', 'r') as f:
                 q.prepare(f.read())
-            q.bindValue(":startTime", startTime)
-            q.bindValue(":endTime", endTime)
-            q.bindValue(":targetGUID", self.source_select.currentData()[1])
+                q.bindValue(":targetGUID", self.source_select.currentData()[1])
+        q.bindValue(":startTime", startTime)
+        q.bindValue(":endTime", endTime)
+        q.exec()
+        self.auras_current = {}
+        while q.next():
+            if q.value(1) not in self.auras_current:
+                self.auras_current[q.value(1)] = {'spellName': q.value(0), 'intervals': []}
+            self.auras_current[q.value(1)]['intervals'].append((q.value(2), q.value(3)))
+        t0 = timeparse(startTime)
+        encounter_length = timeparse(endTime) - t0
+        q.exec("DROP TABLE IF EXISTS temp_buff")
+        q.exec("CREATE TEMP TABLE temp_buff (spellID MEDIUMINT UNSIGNED UNIQUE NOT NULL, spellName VARCHAR(50), count SMALLINT UNSIGNED, uptime TIMESTAMP, uptimepct FLOAT, intervals VARCHAR, maxlen FLOAT, spellSchool TINYINT UNSIGNED, icon VARCHAR(50))")
+        q.prepare("INSERT INTO temp_buff (spellID, spellName, count, uptime, uptimepct, intervals, maxlen, spellSchool, icon) VALUES (:spellID, :spellName, :count, :uptime, :uptimepct, :intervals, :maxlen, :spellSchool, :icon)")
+        q.bindValue(':maxlen', encounter_length.total_seconds())
+        spellInfo = QSqlQuery()
+        spellInfo.prepare("SELECT school_mask, icon FROM spell_db.spell_data WHERE spellID = :spellID")
+        for k in self.auras_current:
+            self.auras_current[k]['count'] = len(self.auras_current[k]['intervals'])
+            self.auras_current[k]['intervals'] = flattenIntervals(self.auras_current[k]['intervals'])
+            self.auras_current[k]['intervals'] = [(timeparse(x[0]), timeparse(x[1])) for x in self.auras_current[k]['intervals']]
+            uptime = datetime.timedelta()
+            for x in self.auras_current[k]['intervals']:
+                uptime += x[1] - x[0]
+            self.auras_current[k]['uptime'] = uptime
+            self.auras_current[k]['uptimepct'] = uptime / encounter_length * 100
+            self.auras_current[k]['intervals'] = [((x[0] - t0).total_seconds(), (x[1] - t0).total_seconds()) for x in self.auras_current[k]['intervals']]
+            q.bindValue(':spellID', k)
+            q.bindValue(':spellName', self.auras_current[k]['spellName'])
+            q.bindValue(':count', self.auras_current[k]['count'])
+            q.bindValue(':uptime', str(self.auras_current[k]['uptime']))
+            q.bindValue(':uptimepct', self.auras_current[k]['uptimepct'])
+            q.bindValue(':intervals', f"{self.auras_current[k]['intervals']}")
+            spellInfo.bindValue(':spellID', k)
+            spellInfo.exec()
+            spellInfo.next()
+            q.bindValue(':spellSchool', spellInfo.value(0))
+            q.bindValue(':icon', spellInfo.value(1))
             q.exec()
-            self.auras_current = {}
-            while q.next():
-                if q.value(1) not in self.auras_current:
-                    self.auras_current[q.value(1)] = {'spellName': q.value(0), 'intervals': []}
-                self.auras_current[q.value(1)]['intervals'].append((q.value(2), q.value(3)))
-            t0 = timeparse(startTime)
-            encounter_length = timeparse(endTime) - t0
-            q.exec("DROP TABLE IF EXISTS temp_buff")
-            q.exec("CREATE TEMP TABLE temp_buff (spellID MEDIUMINT UNSIGNED UNIQUE NOT NULL, spellName VARCHAR(50), count SMALLINT UNSIGNED, uptime TIMESTAMP, uptimepct FLOAT, intervals VARCHAR, maxlen FLOAT, spellSchool TINYINT UNSIGNED, icon VARCHAR(50))")
-            q.prepare("INSERT INTO temp_buff (spellID, spellName, count, uptime, uptimepct, intervals, maxlen, spellSchool, icon) VALUES (:spellID, :spellName, :count, :uptime, :uptimepct, :intervals, :maxlen, :spellSchool, :icon)")
-            q.bindValue(':maxlen', encounter_length.total_seconds())
-            spellInfo = QSqlQuery()
-            spellInfo.prepare("SELECT school_mask, icon FROM spell_db.spell_data WHERE spellID = :spellID")
-            for k in self.auras_current:
-                self.auras_current[k]['count'] = len(self.auras_current[k]['intervals'])
-                self.auras_current[k]['intervals'] = flattenIntervals(self.auras_current[k]['intervals'])
-                self.auras_current[k]['intervals'] = [(timeparse(x[0]), timeparse(x[1])) for x in self.auras_current[k]['intervals']]
-                uptime = datetime.timedelta()
-                for x in self.auras_current[k]['intervals']:
-                    uptime += x[1] - x[0]
-                self.auras_current[k]['uptime'] = uptime
-                self.auras_current[k]['uptimepct'] = uptime / encounter_length * 100
-                self.auras_current[k]['intervals'] = [((x[0] - t0).total_seconds(), (x[1] - t0).total_seconds()) for x in self.auras_current[k]['intervals']]
-                q.bindValue(':spellID', k)
-                q.bindValue(':spellName', self.auras_current[k]['spellName'])
-                q.bindValue(':count', self.auras_current[k]['count'])
-                q.bindValue(':uptime', str(self.auras_current[k]['uptime']))
-                q.bindValue(':uptimepct', self.auras_current[k]['uptimepct'])
-                q.bindValue(':intervals', f"{self.auras_current[k]['intervals']}")
-                spellInfo.bindValue(':spellID', k)
-                spellInfo.exec()
-                spellInfo.next()
-                q.bindValue(':spellSchool', spellInfo.value(0))
-                q.bindValue(':icon', spellInfo.value(1))
-                q.exec()
             display_query = QSqlQuery()
             display_query.exec("SELECT spellName, PRINTF('%.2f%%', uptimepct) AS pct, intervals, count, uptime, spellID, maxlen, spellSchool, icon FROM temp_buff ORDER BY uptime DESC, spellName")
             self.table.setModel(auraSqlTableModel(display_query))
@@ -453,9 +458,9 @@ class MainWindow(QMainWindow):
     
     def updateTargetAffiliation(self):
         meter = self.meter_select.currentText()
-        if meter in (DAMAGEDONE, DAMAGETAKEN):
+        if meter in (DAMAGEDONE, DAMAGETAKEN, DEBUFFS, DEATHS):
             self.target_affiliation = 1 - self.source_affiliation
-        elif meter in (HEALING):
+        elif meter in (HEALING, BUFFS):
             self.target_affiliation = self.source_affiliation
         else:
             self.target_affiliation = 0
---
+++ queries/buffs_taken_all-all.sql
@@ -0,0 +1,23 @@
+SELECT
+    spellName, spellID, COALESCE(MAX(timeStart, :startTime), :startTime), COALESCE(MIN(timeEnd, :endTime), :endTime)
+FROM auras
+JOIN actors
+ON auras.targetGUID = actors.unitGUID
+WHERE
+    auraType = 'BUFF'
+AND (
+        timeStart <= :endTime
+    OR  timeStart IS NULL
+)
+AND (
+        timeEnd >= :startTime
+    OR  timeEnd IS NULL
+)
+AND (
+        actors.isPlayer = :affiliation
+    OR  actors.isPet = :affiliation
+    OR (
+            :affiliation = 0
+        AND actors.isNPC = 1
+    )
+)
